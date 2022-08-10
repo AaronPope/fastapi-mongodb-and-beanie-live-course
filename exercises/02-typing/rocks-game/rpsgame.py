d0f8e901c020ba46a49cfdf325df8bb8135eb04c
Aaron Pope <aaronpope.dev@gmail.com>
2022-08-10 09:05:33 -0400

    added types where appropriate and possible

--- exercises/02-typing/rocks-game/rpsgame.py
+++ exercises/02-typing/rocks-game/rpsgame.py
@@ -3,7 +3,7 @@ import random
 import json
 import os
 
-rolls = {}
+rolls: dict = {}
 
 
 def main():
@@ -13,7 +13,7 @@ def main():
     show_header()
     show_leaderboard()
 
-    # TODO: This doesn't work
+    # NOTE: This doesn't work
     # player1: str, player2: str = get_players()
     player1, player2 = get_players()
 
@@ -33,7 +33,7 @@ def show_header():
 def show_leaderboard():
     leaders: dict[str, int] = load_leaders()
 
-    # TODO: This should be a dict or tuple, but PyCharm insists that it should be a string
+    # NOTE: This should be a dict or tuple, but PyCharm insists that it should be a string
     #   Debug output: sorted_leaders: [('Computer', 1), ('Me', 1)]
     sorted_leaders: list[str] = list(leaders.items())
     sorted_leaders.sort(key=lambda l: l[1], reverse=True)
@@ -48,21 +48,21 @@ def show_leaderboard():
 
 
 def get_players() -> tuple[str, str]:
-    p1 = input("Player 1, what is your name? ")
-    p2 = "Computer"
+    p1: str = input("Player 1, what is your name? ")
+    p2: str = "Computer"
 
     return p1, p2
 
 
-def play_game(player_1: str, player_2: str):
+def play_game(player_1: str, player_2: str) -> None:
     log(f"New game starting between {player_1} and {player_2}.")
 
     wins: dict[str, int] = {player_1: 0, player_2: 0}
     roll_names: list[str] = list(rolls.keys())
 
     while not find_winner(wins, wins.keys()):
-        roll1 = get_roll(player_1, roll_names)
-        roll2 = random.choice(roll_names)
+        roll1: str = get_roll(player_1, roll_names)
+        roll2: str = random.choice(roll_names)
 
         if not roll1:
             print("Try again!")
@@ -75,11 +75,11 @@ def play_game(player_1: str, player_2: str):
         winner = check_for_winning_throw(player_1, player_2, roll1, roll2)
 
         if winner is None:
-            msg = "This round was a tie!"
+            msg: str = "This round was a tie!"
             print(msg)
             log(msg)
         else:
-            msg = f'{winner} takes the round!'
+            msg: str = f'{winner} takes the round!'
             print(msg)
             log(msg)
             wins[winner] += 1
@@ -89,15 +89,15 @@ def play_game(player_1: str, player_2: str):
         log(msg)
         print()
 
-    overall_winner = find_winner(wins, wins.keys())
+    overall_winner: str = find_winner(wins, wins.keys())
     msg = f"{overall_winner} wins the game!"
     print(msg)
     log(msg)
     record_win(overall_winner)
 
 
-def find_winner(wins: dict[str, int], names: dict[str, int]):
-    best_of = 3
+def find_winner(wins: dict[str, int], names: dict[str, int]) -> str | None:
+    best_of: int = 3
     for name in names:
         if wins.get(name, 0) >= best_of:
             return name
@@ -105,7 +105,7 @@ def find_winner(wins: dict[str, int], names: dict[str, int]):
     return None
 
 
-def check_for_winning_throw(player_1: str, player_2: str, roll1, roll2):
+def check_for_winning_throw(player_1: str, player_2: str, roll1, roll2) -> str:
     if roll1 == roll2:
         print("The play was tied!")
         return None
@@ -135,13 +135,14 @@ def get_roll(player_name: str, roll_names):
 
 
 def load_rolls():
+    # NOTE: remind myself of the details of `global`
     global rolls
 
-    directory = os.path.dirname(__file__)
-    filename = os.path.join(directory, 'rolls.json')
+    directory: str = os.path.dirname(__file__)
+    filename: str = os.path.join(directory, 'rolls.json')
 
     with open(filename, 'r', encoding='utf-8') as fin:
-        # TODO: Why won't this work?
+        # NOTE: Why won't this work?
         # rolls: dict[str, dict[str, list[str]]] = json.load(fin)
         #
         # rolls: dict[str, dict[str, list[str]]] = json.load(fin)
@@ -165,7 +166,7 @@ def load_leaders() -> dict[str, int]:
 
 
 def record_win(winner_name):
-    leaders = load_leaders()
+    leaders: dict[str, int] = load_leaders()
 
     if winner_name in leaders:
         leaders[winner_name] += 1
@@ -180,8 +181,8 @@ def record_win(winner_name):
 
 
 def log(msg):
-    directory = os.path.dirname(__file__)
-    filename = os.path.join(directory, 'rps.log')
+    directory: str = os.path.dirname(__file__)
+    filename: str = os.path.join(directory, 'rps.log')
 
     with open(filename, 'a', encoding='utf-8') as fout:
         fout.write(f"[{datetime.datetime.now().date().isoformat()}] ")
